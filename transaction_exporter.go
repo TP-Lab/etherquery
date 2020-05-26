f5493db66e0edcb8da848186a1ebc2906fd465eb
heipacker <heipackermail@gmail.com>
2020-05-27 03:12:28 +0800

    new: 并行处理trace transaction　提升性能

--- transaction_exporter.go
+++ transaction_exporter.go
@@ -15,14 +15,17 @@ import (
 	"math"
 	"math/big"
 	"strings"
+	"sync"
 	"time"
 )
 
 type TransactionExporter struct {
-	appConfig   *AppConfig
-	chainConfig *params.ChainConfig
-	ethereum    *eth.Ethereum
-	saver       Saver
+	appConfig       *AppConfig
+	chainConfig     *params.ChainConfig
+	ethereum        *eth.Ethereum
+	traceConfig     *eth.TraceConfig
+	privateDebugAPI *eth.PrivateDebugAPI
+	saver           Saver
 }
 
 func NewTransactionExporter(appConfig *AppConfig, ethereum *eth.Ethereum) *TransactionExporter {
@@ -40,11 +43,25 @@ func NewTransactionExporter(appConfig *AppConfig, ethereum *eth.Ethereum) *Trans
 			appConfig: appConfig,
 		}
 	}
+	tracerType := "callTracer"
+	traceConfig := &eth.TraceConfig{
+		Tracer: &tracerType,
+		LogConfig: &vm.LogConfig{
+			DisableMemory:  false,
+			DisableStack:   false,
+			DisableStorage: false,
+		},
+		Timeout: &appConfig.Timeout,
+		Reexec:  &appConfig.Reexec,
+	}
+	privateDebugAPI := eth.NewPrivateDebugAPI(ethereum)
 	return &TransactionExporter{
-		appConfig:   appConfig,
-		chainConfig: ethereum.BlockChain().Config(),
-		ethereum:    ethereum,
-		saver:       saver,
+		appConfig:       appConfig,
+		chainConfig:     ethereum.BlockChain().Config(),
+		ethereum:        ethereum,
+		traceConfig:     traceConfig,
+		privateDebugAPI: privateDebugAPI,
+		saver:           saver,
 	}
 }
 
@@ -153,156 +170,164 @@ func (s *TransactionExporter) ExportBlock(block *types.Block) (int64, error) {
 	if block == nil || len(block.Transactions()) == 0 {
 		return 0, nil
 	}
-
-	privateDebugAPI := eth.NewPrivateDebugAPI(s.ethereum)
 	signer := types.MakeSigner(s.chainConfig, block.Number())
 
-	var transactionList []Transaction
-	for i, tx := range block.Transactions() {
-		fromAddress, err := types.Sender(signer, tx)
-		if err != nil {
-			log.Errorf("sender %v error %v", tx.Hash().String(), err)
-			return -1, err
-		}
-		toAddress := tx.To()
-		var to string
-		if toAddress != nil {
-			to = toAddress.String()
-		}
-		transaction := Transaction{
-			Timestamp:        *big.NewInt(int64(block.Time())),
-			BlockNumber:      *block.Number(),
-			TokenValue:       *big.NewInt(0),
-			Value:            *tx.Value(),
-			Hash:             tx.Hash().String(),
-			Nonce:            tx.Nonce(),
-			BlockHash:        block.Hash().String(),
-			TransactionIndex: *big.NewInt(int64(i)),
-			LogIndex:         *LogIndexDefault,
-			InternalIndex:    InternalIndexDefault,
-			From:             fromAddress.String(),
-			To:               to,
-			ContractAddress:  "",
-			TokenType:        TokenTypeDefault,
-			Data:             tx.Data(),
-			Gas:              *big.NewInt(int64(tx.Gas())),
-			GasPrice:         *tx.GasPrice(),
-			UsedGas:          *big.NewInt(int64(tx.Gas())),
-			Status:           TransactionStatusSuccess,
-		}
-		receiptsList, err := s.ethereum.APIBackend.GetReceipts(context.Background(), tx.Hash())
-		if err != nil {
-			log.Errorf("get receipts by %v error %v", tx.Hash().String(), err)
-		}
-		s.parseTransactionTokenInfo(&transaction, &receiptsList)
-		if len(receiptsList) > 0 {
-			marshal1, _ := json.Marshal(receiptsList)
-			log.Infof("receiptsList %v, %v", tx.Hash().String(), string(marshal1))
-			for _, receipt := range receiptsList {
-				transaction.Status = receipt.Status
+	lock := &sync.Mutex{}
+	wg := &sync.WaitGroup{}
+	var result []Transaction
+	for index, _ := range block.Transactions() {
+		wg.Add(1)
+		go func(index int) {
+			defer wg.Done()
 
-				for _, log1 := range receipt.Logs {
-					if len(log1.Topics) <= 0 {
-						continue
-					}
-					eventFunSign := log1.Topics[0].String()
-					//keccak256("Transfer(address,address,uint256)")
-					if !(eventFunSign == "0xddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef" &&
-						len(log1.Topics) == 3) {
-						// 跳过不是transfer转账类型
-						continue
-					}
-					transaction1 := &Transaction{
-						Timestamp: *big.NewInt(int64(block.Time())),
-						Gas:       *big.NewInt(int64(receipt.CumulativeGasUsed)),
-						GasPrice:  *tx.GasPrice(),
-						UsedGas:   *big.NewInt(int64(receipt.GasUsed)),
-						Hash:      tx.Hash().String(),
-						Nonce:     tx.Nonce(),
-						From:      fromAddress.String(),
-						To:        to,
-						Status:    receipt.Status,
-					}
-					topic1 := log1.Topics[1].Bytes()
-					transaction1.From = string(topic1[len(topic1)-40 : len(topic1)])
-					if !strings.HasPrefix(transaction1.From, "0x") {
-						transaction1.From = "0x" + transaction1.From
-					}
-					topic2 := log1.Topics[2].Bytes()
-					transaction1.To = string(topic2[len(topic2)-40 : len(topic2)])
-					if !strings.HasPrefix(transaction1.To, "0x") {
-						transaction1.To = "0x" + transaction1.To
-					}
-					transaction1.ContractAddress = log1.Address.String()
-					transaction1.TokenType = TokenTypeToken
-					transaction1.BlockHash = log1.BlockHash.String()
-					transaction1.BlockNumber = *big.NewInt(int64(log1.BlockNumber))
-					transaction1.TransactionIndex = *big.NewInt(int64(log1.TxIndex))
-					transaction1.LogIndex = *big.NewInt(int64(log1.Index))
-					transaction1.InternalIndex = InternalIndexDefault
-					transaction1.TokenValue.UnmarshalJSON(log1.Data)
-
-					transactionList = append(transactionList, *transaction1)
-				}
+			transactionList, _ := s.processTx(signer, block, index)
+			if len(transactionList) > 0 {
+				func() {
+					lock.Lock()
+					defer lock.Unlock()
+					result = append(result, transactionList...)
+				}()
 			}
-		}
-
-		tracerType := "callTracer"
-		traceConfig := &eth.TraceConfig{
-			Tracer: &tracerType,
-			LogConfig: &vm.LogConfig{
-				DisableMemory:  false,
-				DisableStack:   false,
-				DisableStorage: false,
-			},
-			Timeout: &s.appConfig.Timeout,
-			Reexec:  &s.appConfig.Reexec,
-		}
-
-		var rawMessageInterface interface{}
-		func() {
-			startTime := time.Now().UnixNano()
-			defer func() {
-				elapse := (time.Now().UnixNano() - startTime) / 10e6
-				if elapse > 500 {
-					log.Infof("trace transaction %v elapse time %vms", tx.Hash(), elapse)
-				}
-			}()
-			rawMessageInterface, err = privateDebugAPI.TraceTransaction(context.Background(), tx.Hash(), traceConfig)
-		}()
-		if err != nil {
-			log.Errorf("trace transaction %v error %v", tx.Hash().String(), err)
-			//设置超时状态
-			if strings.Contains(err.Error(), "execution timeout") {
-				transaction.Status = TransactionStatusTimeout
-			}
-		} else {
-			if rawMessageInterface != nil {
-				rawMessage := rawMessageInterface.(json.RawMessage)
-				jsonParsed, err := gabs.ParseJSON(rawMessage)
-				if err != nil {
-					log.Errorf("parse json %v error %v", string(rawMessage), err)
-				} else {
-					//标记当前交易是什么op code
-					if !jsonParsed.ExistsP("type") {
-						transaction.OpCode = jsonParsed.Path("type").String()
-					}
-					if jsonParsed.ExistsP("calls") {
-						log.Debugf("rawMessage %v, %v", tx.Hash().String(), jsonParsed.String())
-						internalIndex := transaction.InternalIndex
-						children, _ := jsonParsed.S("calls").Children()
-						for i, child := range children {
-							newInternalIndex := fmt.Sprintf("%v_%v", internalIndex, i)
-							s.parseRawMessage(newInternalIndex, transaction, block, tx, child, transactionList)
-						}
-					}
-				}
-			}
-		}
-
-		transactionList = append(transactionList, transaction)
+		}(index)
 	}
-	return s.saver.SaveTransactionList(transactionList)
+	wg.Wait()
+	return s.saver.SaveTransactionList(result)
+}
+
+func (s *TransactionExporter) processTx(signer types.Signer, block *types.Block, index int) ([]Transaction, error) {
+	var transactionList []Transaction
+	tx := block.Transactions()[index]
+	fromAddress, err := types.Sender(signer, tx)
+	if err != nil {
+		log.Errorf("sender %v error %v", tx.Hash().String(), err)
+		return nil, err
+	}
+	toAddress := tx.To()
+	var to string
+	if toAddress != nil {
+		to = toAddress.String()
+	}
+	transaction := Transaction{
+		Timestamp:        *big.NewInt(int64(block.Time())),
+		BlockNumber:      *block.Number(),
+		TokenValue:       *big.NewInt(0),
+		Value:            *tx.Value(),
+		Hash:             tx.Hash().String(),
+		Nonce:            tx.Nonce(),
+		BlockHash:        block.Hash().String(),
+		TransactionIndex: *big.NewInt(int64(index)),
+		LogIndex:         *LogIndexDefault,
+		InternalIndex:    InternalIndexDefault,
+		From:             fromAddress.String(),
+		To:               to,
+		ContractAddress:  "",
+		TokenType:        TokenTypeDefault,
+		Data:             tx.Data(),
+		Gas:              *big.NewInt(int64(tx.Gas())),
+		GasPrice:         *tx.GasPrice(),
+		UsedGas:          *big.NewInt(int64(tx.Gas())),
+		Status:           TransactionStatusSuccess,
+	}
+	receiptsList, err := s.ethereum.APIBackend.GetReceipts(context.Background(), tx.Hash())
+	if err != nil {
+		log.Errorf("get receipts by %v error %v", tx.Hash().String(), err)
+	}
+	s.parseTransactionTokenInfo(&transaction, &receiptsList)
+	if len(receiptsList) > 0 {
+		marshal1, _ := json.Marshal(receiptsList)
+		log.Infof("receiptsList %v, %v", tx.Hash().String(), string(marshal1))
+		for _, receipt := range receiptsList {
+			transaction.Status = receipt.Status
+
+			for _, log1 := range receipt.Logs {
+				if len(log1.Topics) <= 0 {
+					continue
+				}
+				eventFunSign := log1.Topics[0].String()
+				//keccak256("Transfer(address,address,uint256)")
+				if !(eventFunSign == "0xddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef" &&
+					len(log1.Topics) == 3) {
+					// 跳过不是transfer转账类型
+					continue
+				}
+				transaction1 := &Transaction{
+					Timestamp: *big.NewInt(int64(block.Time())),
+					Gas:       *big.NewInt(int64(receipt.CumulativeGasUsed)),
+					GasPrice:  *tx.GasPrice(),
+					UsedGas:   *big.NewInt(int64(receipt.GasUsed)),
+					Hash:      tx.Hash().String(),
+					Nonce:     tx.Nonce(),
+					From:      fromAddress.String(),
+					To:        to,
+					Status:    receipt.Status,
+				}
+				topic1 := log1.Topics[1].Bytes()
+				transaction1.From = string(topic1[len(topic1)-40 : len(topic1)])
+				if !strings.HasPrefix(transaction1.From, "0x") {
+					transaction1.From = "0x" + transaction1.From
+				}
+				topic2 := log1.Topics[2].Bytes()
+				transaction1.To = string(topic2[len(topic2)-40 : len(topic2)])
+				if !strings.HasPrefix(transaction1.To, "0x") {
+					transaction1.To = "0x" + transaction1.To
+				}
+				transaction1.ContractAddress = log1.Address.String()
+				transaction1.TokenType = TokenTypeToken
+				transaction1.BlockHash = log1.BlockHash.String()
+				transaction1.BlockNumber = *big.NewInt(int64(log1.BlockNumber))
+				transaction1.TransactionIndex = *big.NewInt(int64(log1.TxIndex))
+				transaction1.LogIndex = *big.NewInt(int64(log1.Index))
+				transaction1.InternalIndex = InternalIndexDefault
+				transaction1.TokenValue.UnmarshalJSON(log1.Data)
+
+				transactionList = append(transactionList, *transaction1)
+			}
+		}
+	}
+
+	var rawMessageInterface interface{}
+	func() {
+		startTime := time.Now().UnixNano()
+		defer func() {
+			elapse := (time.Now().UnixNano() - startTime) / 10e6
+			if elapse > 500 {
+				log.Infof("trace transaction %v elapse time %vms", tx.Hash(), elapse)
+			}
+		}()
+		rawMessageInterface, err = s.privateDebugAPI.TraceTransaction(context.Background(), tx.Hash(), s.traceConfig)
+	}()
+	if err != nil {
+		log.Errorf("trace transaction %v error %v", tx.Hash().String(), err)
+		//设置超时状态
+		if strings.Contains(err.Error(), "execution timeout") {
+			transaction.Status = TransactionStatusTimeout
+		}
+	} else {
+		if rawMessageInterface != nil {
+			rawMessage := rawMessageInterface.(json.RawMessage)
+			jsonParsed, err := gabs.ParseJSON(rawMessage)
+			if err != nil {
+				log.Errorf("parse json %v error %v", string(rawMessage), err)
+			} else {
+				//标记当前交易是什么op code
+				if !jsonParsed.ExistsP("type") {
+					transaction.OpCode = jsonParsed.Path("type").String()
+				}
+				if jsonParsed.ExistsP("calls") {
+					log.Debugf("rawMessage %v, %v", tx.Hash().String(), jsonParsed.String())
+					internalIndex := transaction.InternalIndex
+					children, _ := jsonParsed.S("calls").Children()
+					for i, child := range children {
+						newInternalIndex := fmt.Sprintf("%v_%v", internalIndex, i)
+						s.parseRawMessage(newInternalIndex, transaction, block, tx, child, transactionList)
+					}
+				}
+			}
+		}
+	}
+	transactionList = append(transactionList, transaction)
+
+	return transactionList, nil
 }
 
 func (s *TransactionExporter) parseRawMessage(internalIndex string, parentTransaction Transaction, block *types.Block, tx *types.Transaction, jsonParsed *gabs.Container, transactionList []Transaction) {
